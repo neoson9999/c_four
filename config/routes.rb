7e582a07482d4c28ce830c8a29767b2e51f69565
Lisandro Español <neoson9998@gmail.com>
2015-10-01 21:40:01 +0800

    add redirection to play_game

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
 
   get 'disks/:id/' => 'disks#show'
 
+  root to: 'application#redirect_to_game_page'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
